252dcb55fb044bda481187e49a5502c28bef9703
patrikwwdc <patrik@wwdevcenter.com>
2015-06-08 21:41:24 +0200

    A

--- BasicGame123/src/mygame/Player.java
+++ BasicGame123/src/mygame/Player.java
@@ -27,7 +27,6 @@ public class Player extends Node {
     private TerrainQuad terrain;
     private boolean atTarget; 
    
-    
     public Player(AssetManager assetManager, TerrainQuad terrainTo) {
         terrain = terrainTo;
         playerNode = assetManager.loadModel("Models/template animations9/template animations9.j3o");
